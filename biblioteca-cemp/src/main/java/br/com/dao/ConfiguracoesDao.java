969fe350cf2bff56029c57665d855bfd9e782597
renan <renan@renan-Lenovo-ideapad-320-15IKB>
2019-02-28 16:26:07 -0300

    Fechando conexao de configuracoesDao

--- biblioteca-cemp/src/main/java/br/com/dao/ConfiguracoesDao.java
+++ biblioteca-cemp/src/main/java/br/com/dao/ConfiguracoesDao.java
@@ -4,17 +4,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.hibernate.Criteria;
+import org.hibernate.Session;
 import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Restrictions;
 
 import br.com.proline.model.Configuracoes;
 
 public class ConfiguracoesDao extends DaoGeneric<Configuracoes> {
-	
+
 	public Configuracoes configuracao() {
-		Criteria c = criaCriteria();
+
+		Configuracoes conf = new Configuracoes();
+
+		Session session = criaSession();
+		session.beginTransaction();
+		Criteria c = session.createCriteria(getClasse());
+
 		c.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY);
-		
-		return (Configuracoes) c.uniqueResult();
+
+		conf = (Configuracoes) c.uniqueResult();
+
+		session.getTransaction().commit();
+		session.close();
+
+		return conf;
 	}
 }
